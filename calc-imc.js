43e7d62944a448eb0cd54c19330bc600303b4aef
Maria Camargo <madecamargo@outlook.com>
2019-11-12 22:09:25 -0300

    Adicionadas as devidas pontuacoes.

--- calc-imc.js
+++ calc-imc.js
@@ -2,11 +2,11 @@ const nome = 'Guilherme';
 const peso = 61;
 const altura = 1.61;
 
-const imc = peso / (altura * altura)
+const imc = peso / (altura * altura);
 
 if (imc >= 30){
-    console.log('Guilherme, você está com sobrepeso;')
+    console.log('Guilherme, você está com sobrepeso.');
 }
 else {
-    console.log('Guilherme, você não está com sobrepeso;')
+    console.log('Guilherme, você não está com sobrepeso.');
 }
